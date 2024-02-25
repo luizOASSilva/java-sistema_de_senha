3f2ca2bfb6d1cca95e9c711d269d31aa1b19d9a5
Luiz Silva <160427605+luizOASSilva@users.noreply.github.com>
2024-02-25 04:51:52 -0300

    Add files via upload

---
+++ src/recepcaosus/Recepcaosus.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recepcaosus;
+
+import tela.telarec;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Recepcaosus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        telarec main = new telarec();
+        main.setVisible(true);
+    }
+    
+}
